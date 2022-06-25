696b459793c8382a6632abe6912842233fdce407
Rafael Lima <rafaa99@gmail.com>
2022-06-25 17:25:29 -0300

    using match expressions and how to cover "all" cases

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,38 @@ fn add(num_one: i32, num_two: i32) -> i32 {
 }
 
 fn main() {
-    let total = add(10, 45);
+    let mut total = add(10, 45);
+    let mut free_shipping = false;
 
     if total > 50 {
         println!("You qualify for free shipping!!");
-        println!("Total: {}", total - 10);
+        free_shipping = true;
+    } else if total > 20 {
+        println!("Add more items and you will have free shipping.");
     } else {
-        println!("Total: {:?}", total);
+        println!("No free shipping.");
     }
+
+    match free_shipping {
+        //exhaustive variables
+        true => total = total + 0,
+        false => total = total + 10,
+    }
+
+    // or it can be written like:
+
+    // total = match free_shipping {
+    //     //exhaustive variables
+    //     true => total + 0,
+    //     false => total + 10,
+    // };
+
+    // to print wild cards
+    match total {
+        1 => println!("1"),
+        2 => println!("2"),
+        _ => println!("No match found."),
+    }
+
+    println!("Total: {:?}", total);
 }

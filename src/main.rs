cb44c165fd0aa6171c81ba1ca56c32c410bbdbec
Rafael Lima <rafaa99@gmail.com>
2022-06-25 18:04:19 -0300

    learn about vectors and how to use them

--- src/main.rs
+++ src/main.rs
@@ -40,4 +40,15 @@ fn main() {
 
     let items: [i32; 5] = [1, 2, 3, 4, 5];
     println!("Items: {:?}", items);
+
+    let vector_items: Vec<i32> = vec![1, 2, 3, 4, 5];
+    let mut vector_items_2 = Vec::new();
+    vector_items_2.push(1);
+    vector_items_2.push(2);
+    vector_items_2.push(3);
+    vector_items_2.push(4);
+    vector_items_2.push(5);
+
+    println!("Vector Items: {:?}", vector_items);
+    println!("Vector Items 2: {:?}", vector_items_2);
 }

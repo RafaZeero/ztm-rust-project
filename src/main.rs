3ea1d7f22236f6151c3163e5de8ec857eed721d9
Rafael Lima <rafaa99@gmail.com>
2022-06-25 17:04:30 -0300

    those are macros

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,13 @@ fn add(num_one: i32, num_two: i32) -> i32 {
 }
 
 fn main() {
-    let foo = add(1, 2);
+    let foo = add(10, 2);
+    // how to print a variable
+    println!("first print: {}", foo);
+    //how to print more than one variable
+    println!("second print: {} {}", foo, true);
+    // how to print one variable multiple times OR how to select which variable goes where
+    println!("third print: {0} {0}", foo);
+    // complex placeholder
+    println!("fourth print: {:?}", foo);
 }
